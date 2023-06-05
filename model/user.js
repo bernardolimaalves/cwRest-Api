b527f4798acfc60ab0948726acf802166a8dfa5d
bernardolimaalves <133880302+bernardolimaalves@users.noreply.github.com>
2023-06-05 11:09:33 -0300

    Update user.js

--- model/user.js
+++ model/user.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
+const Books = require('./books');
 
+const User = new mongoose.Schema({
+    name: { 
+        type: String,
+        required: true
+    },
+    email: { 
+        type: String,
+        required: true
+    },
+    booksRead: Array
+});
+
+module.exports = mongoose.model('UserDB', User), User
